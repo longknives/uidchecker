ecc0d389d231bd0f57fae12d59ade2b0bef5205d
longknives <76183876+longknives@users.noreply.github.com>
2021-05-11 22:19:08 -0400

    Update dash.py
    Just enter the name of the user and the information should appear
    Updates
    ---------
    Users can pick if they want filtered or garbage information

--- dash.py
+++ dash.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from threading import Timer
 import time
 from tkinter.constants import COMMAND, END, FALSE
@@ -16,20 +17,56 @@ import urllib
 import urllib3
 import pyautogui
 import requests
+from datetime import date
 
 
 
-window = tk.Tk()
+
+
+
+
+Current_Date = datetime.now()
+
+
+
+
+
+window = tk.Tk(className= "dash")
+
+
+
 
 buttonClicked = False
 
 
-window.title("dash(BETA)") # title
+
 window.minsize(600,400) # window size
+
+info = False
+
+def allinfo(info):
+    if info == True:
+         getresult = name.get()
+
+    the_word = "user_id"
+
+    x1 = "https://forum.neverlose.cc/u/" + getresult +".json"
+    r = requests.get(x1, allow_redirects=False)
+    soup = BeautifulSoup(r.content, 'lxml')
+    words = soup.find(text=lambda text: text and the_word in text)
+
+
+    string = words
+    print("--------------------------------")
+
+
+    print(string)
 
 
  
 def gui(buttonClicked):
+
+
 
 
    if(buttonClicked == True):
@@ -44,13 +81,95 @@ def gui(buttonClicked):
 
 
     string = words
-    print("Important information" + string[0:1000])
-    print(" ")
-    print(" ")
-    print(" ")
-    print("ALL INFORMATION")
-    print(string)
 
+    if(getresult.lower() == "thaiti"):
+        print(name.get() + " is a harmless bitch that cant do anything~!")
+
+    if (name.get().lower() == "longknives"):
+        print("I like longknives")
+
+
+
+
+  
+
+
+
+
+
+    location = string.find("user_id", 0)
+    admin = string.find("admin", 0)
+    granted = string.find("granted_at", 0)
+    email = (string.find("email", 0 ))
+    
+
+    falseadmin = string[admin+7 : admin+12]
+
+    falseemail = string[email+7:email+12]
+
+    location1 = string[location+9:location + 15]
+
+    locationgranted = string[granted+ 13: granted+23]
+
+    acountcreation = string[granted+53: granted + 63]
+
+   
+
+    
+
+    print("--------------------------------")
+
+    print("UID: " + location1)
+
+    if(falseadmin == "false"):
+        print(name.get() + " is not an admin")
+    if(falseadmin == "true"):
+        print(name.get() + " is an admin")
+
+    if(falseemail == "false"):
+            print(name.get() + " no email is associated with the account")
+    else:
+        print(name.get() + " phone number is: " + string[falseemail : falseemail + 20])
+
+
+
+
+    
+
+
+
+
+
+
+
+  
+
+
+    
+
+
+ 
+
+    print("Granted at: " + locationgranted)
+
+    print("Acount created at: " + acountcreation)
+
+    
+
+ 
+
+
+   
+
+
+
+
+
+
+
+
+
+   
 
     
 
@@ -63,8 +182,8 @@ def gui(buttonClicked):
 
  
 name = tk.StringVar()
-nameEntered = ttk.Entry(window, width = 15, textvariable = name)# box
-nameEntered.grid(column = 10, row = 1) # location
+nameEntered = ttk.Entry(window, width = 15, textvariable = name, background='blue')
+nameEntered.grid(column = 10, row = 1) 
 
 
  
@@ -72,21 +191,38 @@ nameEntered.grid(column = 10, row = 1) # location
 
 button = ttk.Button(window, text = "Enter", command=lambda buttonClicked = True: gui(buttonClicked))
              
-button.grid(column= 15, row = 1) # button
+button.grid(column= 15, row = 1) 
 
 
 
 
 
-label = ttk.Label(window, text = "user_id shows uid~!") # text
+
+
+
+
+label = ttk.Label(window, text = "Check Console for other details: ") # text
 label.grid(column = 10, row = 20) 
 
 
+button = ttk.Button(window, text = "All Info", command=lambda info = True: allinfo(info))
+             
+button.grid(column= 20, row = 1) 
 
 
  
 
+
+
+
+
 window.mainloop()
+
+
+       
+
+
+
 
 
        
